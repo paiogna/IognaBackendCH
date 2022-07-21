1161d6f4f54ad033537da77129acd5f53fb8bfee
Patricia <paiogna@gmail.com>
2022-07-21 19:42:14 -0300

    Desafío manejo de archivos en JavaScript

---
+++ DesafioManejoArchivosJS/contenedor.js
@@ -0,0 +1,71 @@
+const fs = require('fs')
+
+class Contenedor {
+    constructor(ruta) {
+        this.ruta=ruta
+    }
+
+async save(obj){ 
+    try {             
+        let dataArch = await fs.promises.readFile(this.ruta, 'utf8') 
+        let dataArchParse = JSON.parse(dataArch) 
+        if (dataArchParse.length) {
+            await fs.promises.writeFile(this.ruta, JSON.stringify( [...dataArchParse, { ...obj, id: dataArchParse[dataArchParse.length - 1].id + 1 } ], null, 2))                              
+        } else {
+            await fs.promises.writeFile(this.ruta, JSON.stringify( [{ ...obj, id: 1 }], null, 2))                
+        }
+        console.log(`El archivo tiene el id: ${dataArchParse[dataArchParse.length - 1].id + 1}`)
+    } catch (error) {
+        console.log(error)
+    }            
+}
+
+async getById(id){
+    try {
+        let dataArch = await fs.promises.readFile(this.ruta, 'utf8')
+        let dataArchParse = JSON.parse(dataArch)
+        let producto = dataArchParse.find(producto => producto.id === id)
+        if (producto) {                
+            console.log(producto)
+        } else {               
+            console.log('No se encontro el producto')  
+        }           
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async getAll(){
+    try {
+        let dataArch = await fs.promises.readFile(this.ruta, 'utf8')
+        let dataArchParse = JSON.parse(dataArch)
+        if (dataArchParse.length) {
+            console.log(dataArchParse)                
+        } else {
+            console.log('No hay productos')
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async delete(id){
+    let dataArch = await fs.promises.readFile(this.ruta, 'utf8')
+    let dataArchParse = JSON.parse(dataArch)
+    let producto = dataArchParse.find(prod => prod.id === id)
+    if (producto) {
+        const dataArchParseFiltrado = dataArchParse.filter(prod => prod.id !== id)
+        await fs.promises.writeFile(this.ruta, JSON.stringify(dataArchParseFiltrado, null, 2), 'utf-8')
+        console.log('Producto eliminado')
+    }else{
+        console.log('No existe el producto')
+    }
+}
+
+async deleteAll(){
+    await fs.promises.writeFile(this.ruta, JSON.stringify([], null, 2), 'utf-8')
+}
+}
+
+
+module.exports = Contenedor
---
+++ DesafioManejoArchivosJS/main.js
@@ -0,0 +1,14 @@
+const Contenedor = require("./contenedor");
+
+
+const contenedor = new Contenedor('./prueba.txt')
+
+// contenedor.save({ nombre: 'Royal Canin', precio: 3500, categoria: 'Alimento', descripcion: 'Alimento para perros' })
+
+// contenedor.getById(6)
+
+// contenedor.getAll()
+
+// contenedor.delete(3)
+
+// contenedor.deleteAll()
